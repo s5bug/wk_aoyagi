b0c40d9c3cae7538e57f2ab5eb0fba721a9db191
Aly <me@s5.pm>
2025-06-02 13:53:35 -0700

    Check for WKOF to support burned items

--- wkaoyagi.user.js
+++ wkaoyagi.user.js
@@ -8,7 +8,7 @@
 // @include     https://www.wanikani.com/dashboard
 // @match       https://www.wanikani.com/subjects/review*
 // @match       https://www.wanikani.com/subjects/extra_study*
-// @version     1.0
+// @version     1.1
 // @license     MIT
 // @grant       GM_getResourceURL
 // @grant       window.onurlchange
@@ -47,52 +47,64 @@
 
         if (!id_srs_element) {
             console.error("Failed to find the expected script element with data-quiz-queue-target=\"subjectIdsWithSRS\"");
-            return;
-        }
+        } else {
+            try {
+                const id_srs_data = JSON.parse(id_srs_element.innerHTML);
 
+                // Extract the relevant part of the data
+                const id_srs_array = id_srs_data.subject_ids_with_srs_info;
 
-        try {
-            const id_srs_data = JSON.parse(id_srs_element.innerHTML);
-
-            // Extract the relevant part of the data
-            const id_srs_array = id_srs_data.subject_ids_with_srs_info;
-
-            if (Array.isArray(id_srs_array)) {
-                // Convert to a mapping of id => srs stage
-                for (const [id, stage, _] of id_srs_array) {
-                    id_to_srs[id] = stage;
+                if (Array.isArray(id_srs_array)) {
+                    // Convert to a mapping of id => srs stage
+                    for (const [id, stage, _] of id_srs_array) {
+                        id_to_srs[id] = stage;
+                    }
+                } else {
+                    console.error("Parsed data is not in the expected format:", id_srs_data);
+                    return;
                 }
-            } else {
-                console.error("Parsed data is not in the expected format:", id_srs_data);
+            } catch (error) {
+                console.error("Error parsing JSON or converting entries:", error);
                 return;
             }
-        } catch (error) {
-            console.error("Error parsing JSON or converting entries:", error);
-            return;
         }
-
-
-        window.addEventListener("willShowNextQuestion", e => {
-            if (e.detail && e.detail.subject && e.detail.subject.id) {
-                const item_id = e.detail.subject.id;
-                const srs = id_to_srs[item_id] || 0;
-                if (srs >= 8) {
-                    // always be sousho
-                    ruleFns[2]()
-                } else if (srs >= 7) {
-                    // randomize between gyousho or sousho
-                    ruleFns[1 + Math.floor(Math.random() * 2)]()
-                } else if (srs >= 5) {
-                    ruleFns[Math.floor(Math.random() * 2)]()
-                } else {
-                    ruleFns[0]()
-                }
-            } else {
-                console.error("Invalid event detail structure:", e.detail);
-            }
-        });
     }
 
-    window.addEventListener('urlchange', reRunOnPageChange)
-    reRunOnPageChange()
+    window.addEventListener("willShowNextQuestion", e => {
+        if (e.detail && e.detail.subject && e.detail.subject.id) {
+            const item_id = e.detail.subject.id;
+            const srs = id_to_srs[item_id] || 0;
+            if (srs >= 8) {
+                // always be sousho
+                ruleFns[2]()
+            } else if (srs >= 7) {
+                // randomize between gyousho or sousho
+                ruleFns[1 + Math.floor(Math.random() * 2)]()
+            } else if (srs >= 5) {
+                ruleFns[Math.floor(Math.random() * 2)]()
+            } else {
+                ruleFns[0]()
+            }
+        } else {
+            console.error("Invalid event detail structure:", e.detail);
+        }
+    });
+
+    if(wkof) {
+        wkof.include('ItemData')
+        wkof.ready('ItemData')
+            .then(() => wkof.ItemData.get_items({wk_items: {options: {assignments: true}}}))
+            .then(items => {
+                for (const item of items) {
+                    if(item.assignments) {
+                        id_to_srs[item.id] = item.assignments.srs_stage;
+                    }
+                }
+                window.addEventListener('urlchange', reRunOnPageChange)
+                reRunOnPageChange()
+            })
+    } else {
+        window.addEventListener('urlchange', reRunOnPageChange)
+        reRunOnPageChange()
+    }
 })();
