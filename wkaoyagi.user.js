9cbb1595b376cf71a11457647e806f6baedf5e09
Aly <me@s5.pm>
2026-03-14 00:43:27 -0600

    just use wkof because it caches

--- wkaoyagi.user.js
+++ wkaoyagi.user.js
@@ -3,11 +3,8 @@
 // @author      Aly
 // @description Switches font based on difficulty of WK review item
 // @run-at      document-end
-// @include     https://www.wanikani.com
-// @include     https://www.wanikani.com/
-// @include     https://www.wanikani.com/dashboard
-// @match       https://www.wanikani.com/subjects/review*
-// @match       https://www.wanikani.com/subjects/extra_study*
+// @match       https://www.wanikani.com/*
+// @match       https://preview.wanikani.com/*
 // @version     1.1
 // @license     MIT
 // @grant       GM_getResourceURL
@@ -43,34 +40,22 @@
 
     const reRunOnPageChange = () => {
         // Create object mapping item id to srs stage
-        const id_srs_element = document.querySelector("script[data-quiz-queue-target=\"subjectIdsWithSRS\"]");
-
-        if (!id_srs_element) {
-            console.error("Failed to find the expected script element with data-quiz-queue-target=\"subjectIdsWithSRS\"");
-        } else {
-            try {
-                const id_srs_data = JSON.parse(id_srs_element.innerHTML);
-
-                // Extract the relevant part of the data
-                const id_srs_array = id_srs_data.subject_ids_with_srs_info;
-
-                if (Array.isArray(id_srs_array)) {
-                    // Convert to a mapping of id => srs stage
-                    for (const [id, stage, _] of id_srs_array) {
-                        id_to_srs[id] = stage;
+        if(wkof) {
+            wkof.include('ItemData')
+            wkof.ready('ItemData')
+                .then(() => wkof.ItemData.get_items({wk_items: {options: {assignments: true}}}))
+                .then(items => {
+                    for (const item of items) {
+                        if(item.assignments) {
+                            id_to_srs[item.id] = item.assignments.srs_stage;
+                        }
                     }
-                } else {
-                    console.error("Parsed data is not in the expected format:", id_srs_data);
-                    return;
-                }
-            } catch (error) {
-                console.error("Error parsing JSON or converting entries:", error);
-                return;
-            }
+                })
         }
     }
 
     window.addEventListener("willShowNextQuestion", e => {
+        console.log("AAAAAA")
         if (e.detail && e.detail.subject && e.detail.subject.id) {
             const item_id = e.detail.subject.id;
             const srs = id_to_srs[item_id] || 0;
@@ -90,21 +75,6 @@
         }
     });
 
-    if(wkof) {
-        wkof.include('ItemData')
-        wkof.ready('ItemData')
-            .then(() => wkof.ItemData.get_items({wk_items: {options: {assignments: true}}}))
-            .then(items => {
-                for (const item of items) {
-                    if(item.assignments) {
-                        id_to_srs[item.id] = item.assignments.srs_stage;
-                    }
-                }
-                window.addEventListener('urlchange', reRunOnPageChange)
-                reRunOnPageChange()
-            })
-    } else {
-        window.addEventListener('urlchange', reRunOnPageChange)
-        reRunOnPageChange()
-    }
+    window.addEventListener('urlchange', reRunOnPageChange)
+    reRunOnPageChange()
 })();
